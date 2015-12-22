b70409e72f2ac1d91a34a2a1b243c532ba1e54aa
luqin <h-io@qq.com>
2015-12-22 23:51:43 +0800

    demo

--- examples/src/js/components/App.js
+++ examples/src/js/components/App.js
@@ -12,6 +12,8 @@ import {
 } from 'react-bootstrap';
 import DatetimeRangePicker from 'react-bootstrap-datetimerangepicker';
 
+moment.locale('zh-cn');
+
 class App extends React.Component {
 
   constructor(props) {
@@ -41,16 +43,37 @@ class App extends React.Component {
   }
 
   render() {
-    var start = this.state.startDate.format('YYYY-MM-DD');
-    var end = this.state.endDate.format('YYYY-MM-DD');
-    var label = start + ' - ' + end;
+    let start = this.state.startDate.format('YYYY-MM-DD');
+    let end = this.state.endDate.format('YYYY-MM-DD');
+    let label = start + ' - ' + end;
     if (start === end) {
       label = start;
     }
 
+    let locale = {
+      format: 'YYYY-MM-DD HH-mm-ss',
+      separator: ' - ',
+      applyLabel: '确定',
+      cancelLabel: '取消',
+      weekLabel: 'W',
+      customRangeLabel: 'Custom Range',
+      daysOfWeek: moment.weekdaysMin(),
+      monthNames: moment.monthsShort(),
+      firstDay: moment.localeData().firstDayOfWeek(),
+    };
+
     return (
       <Grid>
         <Row>
+          <Col md={3}>
+            <DatetimeRangePicker
+              startDate={this.state.startDate}
+              endDate={this.state.endDate}
+              onEvent={this.handleEvent}
+            >
+              <input type="text" name="daterange" value={label}/>
+            </DatetimeRangePicker>
+          </Col>
           <Col md={3}>
             <DatetimeRangePicker
               startDate={this.state.startDate}
@@ -64,7 +87,27 @@ class App extends React.Component {
 									<span>
 										{label}
 									</span>
-                  <span className="caret"></span>
+                  <span className="caret"/>
+                </div>
+              </Button>
+            </DatetimeRangePicker>
+          </Col>
+          <Col md={3}>
+            <DatetimeRangePicker
+              timePicker
+              timePickerSeconds
+              startDate={this.state.startDate}
+              endDate={this.state.endDate}
+              locale={locale}
+              onEvent={this.handleEvent}
+            >
+              <Button className="selected-date-range-btn" style={{ width: '100%' }}>
+                <div className="pull-left"><Glyphicon glyph="calendar"/></div>
+                <div className="pull-right">
+									<span>
+										{label}
+									</span>
+                  <span className="caret"/>
                 </div>
               </Button>
             </DatetimeRangePicker>
