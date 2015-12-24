ee332e8996a9083bb78d66d6735942ebf38ac2ff
luqin <h-io@qq.com>
2015-12-24 22:14:06 +0800

    fix lint error

--- examples/src/js/components/examples/DateAndTime.js
+++ examples/src/js/components/examples/DateAndTime.js
@@ -22,7 +22,7 @@ class DateAndTime extends React.Component {
         'Last 7 Days': [moment().subtract(6, 'days'), moment()],
         'Last 30 Days': [moment().subtract(29, 'days'), moment()],
         'This Month': [moment().startOf('month'), moment().endOf('month')],
-        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
       },
     };
   }
